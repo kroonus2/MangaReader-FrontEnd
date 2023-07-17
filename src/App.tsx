17832859093bd9ceefca5e1684ec760b5d238a80
kroonus2 <87575645+kroonus2@users.noreply.github.com>
2023-07-17 19:46:52 -0300

    Adicionando responsividade aos componentes criados

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import React from "react";
+import { Footerbar } from "./components/Footerbar";
 import { Navbar } from "./components/Navbar";
 import { MainRoutes } from "./routes/MainRoutes";
 
 export default function App() {
   return (
-    <div className="bg-gray-700 overflow-auto">
+    <div className="container m-auto bg-gray-700 min-h-screen overflow-x-hidden flex flex-col relative">
       <Navbar />
-      <MainRoutes />
+      <div className="flex-grow">
+        <MainRoutes />
+      </div>
+      <div className="">
+        <Footerbar />
+      </div>
     </div>
-  )
+  );
 }
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,86 +1,97 @@
 export const MainPage = () => {
     return (
-        <div className="container mx-auto h-screen px-8 py-2 grid grid-cols-3 gap-4 text-white h-fit">
-            <div className="col-span-2 p-2 border-2 border-sky-700 rounded-lg">
+        <div className="container mx-auto h-screen px-8 py-2  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 text-white">
+            <div className="lg:col-span-2 p-2 border-2 border-sky-700 rounded-lg overflow-x-auto items-center">
                 <h2>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores earum sunt atque minima doloribus illo nostrum. Deleniti nisi officiis quasi sint atque, doloremque quibusdam unde quaerat amet blanditiis minus hic.
                 </h2>
-                <nav className="mx-28 p-9">
-                    <div className="grid grid-flow-col auto-cols-max">
+                <nav className="my-4">
+                    <div className="flex flex-wrap lg:flex-nowrap lg:justify-center md:justify-start">
                         <div>
-                            <button type="button" className="lex flex-shrink-0 items-center bg-sky-700 text-white rounded-md px-5 py-3 hover:shadow-lg text-sm font-medium  mx-3">Todos</button>
+                            <button type="button" className="flex-1 flex-shrink-0 items-center bg-sky-700 text-white rounded-md px-5 py-3 hover:shadow-lg text-sm font-medium mx-3">Todos</button>
                         </div>
                         <div>
-                            <button type="button" className="lex flex-shrink-0 items-center bg-sky-700 text-white rounded-md px-5 py-3 hover:shadow-lg text-sm font-medium  mx-3">Mangás</button>
+                            <button type="button" className="flex-1 flex-shrink-0 items-center bg-sky-700 text-white rounded-md px-5 py-3 hover:shadow-lg text-sm font-medium mx-3">Mangás</button>
                         </div>
                         <div>
-                            <button type="button" className="lex flex-shrink-0 items-center bg-sky-700 text-white rounded-md px-5 py-3 hover:shadow-lg text-sm font-medium  mx-3">Manhuas</button>
+                            <button type="button" className="flex-1 flex-shrink-0 items-center bg-sky-700 text-white rounded-md px-5 py-3 hover:shadow-lg text-sm font-medium mx-3">Manhuas</button>
                         </div>
                         <div>
-                            <button type="button" className="lex flex-shrink-0 items-center bg-sky-700 text-white rounded-md px-5 py-3 hover:shadow-lg text-sm font-medium  mx-3">Web-Toons</button>
+                            <button type="button" className="flex-1 flex-shrink-0 items-center bg-sky-700 text-white rounded-md px-5 py-3 hover:shadow-lg text-sm font-medium mx-3">Web-Toons</button>
                         </div>
                         <div>
-                            <button type="button" className="lex flex-shrink-0 items-center bg-sky-700 text-white rounded-md px-5 py-3 hover:shadow-lg text-sm font-medium  mx-3">Web-Novels</button>
+                            <button type="button" className="flex-1 flex-shrink-0 items-center bg-sky-700 text-white rounded-md px-5 py-3 hover:shadow-lg text-sm font-medium mx-3">Web-Novels</button>
                         </div>
                     </div>
                 </nav>
                 <div className="flex flex-1 flex-wrap justify-center">
-                    <div className="card w-52 bg-gray-700 shadow-xl p-6 hover:shadow-2xl hover:shadow-sky-500/40 border-2 border-sky-700 rounded-xl m-2">
-                        <figure><img src="https://placehold.co/150x200" /></figure>
+                    <div className="card w-full sm:w-52 bg-gray-700 shadow-xl p-6 hover:shadow-2xl hover:shadow-sky-500/40 border-2 border-sky-700 rounded-xl m-2">
+                        <div className="flex flex-col items-center"> {/* Centralize a imagem */}
+                            <figure>
+                                <img src="https://placehold.co/150x200" alt="img-front-manga" />
+                            </figure>
+                        </div>
                         <div className="card-body p-2">
                             <h2 className="card-title text-center">Shoes!</h2>
-                            <p className="">If a dog chews shoes whose shoes does he choose?</p>
+                            <p className="text-sky-500">If a dog chews shoes whose shoes does he choose?</p>
                         </div>
                     </div>
-                    <div className="card w-52 bg-gray-700 shadow-xl p-6 hover:shadow-2xl hover:shadow-sky-500/40 border-2 border-sky-700 rounded-xl m-2" >
-                        <figure><img src="https://placehold.co/150x200" /></figure>
+                    <div className="card w-full sm:w-52 bg-gray-700 shadow-xl p-6 hover:shadow-2xl hover:shadow-sky-500/40 border-2 border-sky-700 rounded-xl m-2">
+                        <div className="flex flex-col items-center"> {/* Centralize a imagem */}
+                            <figure>
+                                <img src="https://placehold.co/150x200" alt="img-front-manga" />
+                            </figure>
+                        </div>
                         <div className="card-body p-2">
                             <h2 className="card-title text-center">Shoes!</h2>
-                            <p className="">If a dog chews shoes whose shoes does he choose?</p>
+                            <p className="text-sky-500">If a dog chews shoes whose shoes does he choose?</p>
                         </div>
                     </div>
-                    <div className="card w-52 bg-gray-700 shadow-xl p-6 hover:shadow-2xl hover:shadow-sky-500/40 border-2 border-sky-700 rounded-xl m-2">
-                        <figure><img src="https://placehold.co/150x200" /></figure>
+                    <div className="card w-full sm:w-52 bg-gray-700 shadow-xl p-6 hover:shadow-2xl hover:shadow-sky-500/40 border-2 border-sky-700 rounded-xl m-2">
+                        <div className="flex flex-col items-center"> {/* Centralize a imagem */}
+                            <figure>
+                                <img src="https://placehold.co/150x200" alt="img-front-manga" />
+                            </figure>
+                        </div>
                         <div className="card-body p-2">
                             <h2 className="card-title text-center">Shoes!</h2>
-                            <p className="">If a dog chews shoes whose shoes does he choose?</p>
+                            <p className="text-sky-500">If a dog chews shoes whose shoes does he choose?</p>
                         </div>
                     </div>
-                    <div className="card w-52 bg-gray-700 shadow-xl p-6 hover:shadow-2xl hover:shadow-sky-500/40 border-2 border-sky-700 rounded-xl m-2">
-                        <figure><img src="https://placehold.co/150x200" /></figure>
+                    <div className="card w-full sm:w-52 bg-gray-700 shadow-xl p-6 hover:shadow-2xl hover:shadow-sky-500/40 border-2 border-sky-700 rounded-xl m-2">
+                        <div className="flex flex-col items-center"> {/* Centralize a imagem */}
+                            <figure>
+                                <img src="https://placehold.co/150x200" alt="img-front-manga" />
+                            </figure>
+                        </div>
                         <div className="card-body p-2">
                             <h2 className="card-title text-center">Shoes!</h2>
-                            <p className="">If a dog chews shoes whose shoes does he choose?</p>
+                            <p className="text-sky-500">If a dog chews shoes whose shoes does he choose?</p>
                         </div>
                     </div>
-                    <div className="card w-52 bg-gray-700 shadow-xl p-6 hover:shadow-2xl hover:shadow-sky-500/40 border-2 border-sky-700 rounded-xl m-2">
-                        <figure><img src="https://placehold.co/150x200" /></figure>
+                    <div className="card w-full sm:w-52 bg-gray-700 shadow-xl p-6 hover:shadow-2xl hover:shadow-sky-500/40 border-2 border-sky-700 rounded-xl m-2">
+                        <div className="flex flex-col items-center"> {/* Centralize a imagem */}
+                            <figure>
+                                <img src="https://placehold.co/150x200" alt="img-front-manga" />
+                            </figure>
+                        </div>
                         <div className="card-body p-2">
                             <h2 className="card-title text-center">Shoes!</h2>
-                            <p className="">If a dog chews shoes whose shoes does he choose?</p>
+                            <p className="text-sky-500">If a dog chews shoes whose shoes does he choose?</p>
                         </div>
                     </div>
-                    <div className="card w-52 bg-gray-700 shadow-xl p-6 hover:shadow-2xl hover:shadow-sky-500/40 border-2 border-sky-700 rounded-xl m-2">
-                        <figure><img src="https://placehold.co/150x200" /></figure>
+                    <div className="card w-full sm:w-52 bg-gray-700 shadow-xl p-6 hover:shadow-2xl hover:shadow-sky-500/40 border-2 border-sky-700 rounded-xl m-2">
+                        <div className="flex flex-col items-center"> {/* Centralize a imagem */}
+                            <figure>
+                                <img src="https://placehold.co/150x200" alt="img-front-manga" />
+                            </figure>
+                        </div>
                         <div className="card-body p-2">
                             <h2 className="card-title text-center">Shoes!</h2>
-                            <p className="">If a dog chews shoes whose shoes does he choose?</p>
+                            <p className="text-sky-500">If a dog chews shoes whose shoes does he choose?</p>
                         </div>
                     </div>
-                    <div className="card w-52 bg-gray-700 shadow-xl p-6 hover:shadow-2xl hover:shadow-sky-500/40 border-2 border-sky-700 rounded-xl m-2">
-                        <figure><img src="https://placehold.co/150x200" /></figure>
-                        <div className="card-body p-2">
-                            <h2 className="card-title text-center">Shoes!</h2>
-                            <p className="">If a dog chews shoes whose shoes does he choose?</p>
-                        </div>
-                    </div>
-                    <div className="card w-52 bg-gray-700 shadow-xl p-6 hover:shadow-2xl hover:shadow-sky-500/40 border-2 border-sky-700 rounded-xl m-2">
-                        <figure><img src="https://placehold.co/150x200" /></figure>
-                        <div className="card-body p-2">
-                            <h2 className="card-title text-center">Shoes!</h2>
-                            <p className="">If a dog chews shoes whose shoes does he choose?</p>
-                        </div>
-                    </div>
+
                 </div>
             </div>
             <div className="p-2 border-2 border-sky-700 rounded-lg">
