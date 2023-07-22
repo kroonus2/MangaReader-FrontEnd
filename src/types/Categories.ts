75a0aec681e1ea9f75cf975cb442dbb2bdb36aad
kroonus2 <87575645+kroonus2@users.noreply.github.com>
2023-07-21 21:27:55 -0300

    Create Categories.ts

---
+++ src/types/Categories.ts
@@ -0,0 +1,7 @@
+export const categories = [
+    { id: 1, name: 'Ação', value: 'acao' },
+    { id: 2, name: 'Aventura', value: 'aventura' },
+    { id: 3, name: 'Comédia', value: 'comedia' },
+    { id: 4, name: 'Drama', value: 'drama' },
+    // Adicione mais categorias conforme necessário
+];
