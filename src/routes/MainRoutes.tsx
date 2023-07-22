73f6c47995c1f40ad6f7a77ab4f3c2fd00b34fa8
kroonus2 <87575645+kroonus2@users.noreply.github.com>
2023-07-21 21:27:48 -0300

    Update MainRoutes.tsx

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -3,12 +3,14 @@ import { NotFound } from '../pages/NotFound';
 import { MainPage } from '../pages/MainPage';
 import { Login } from '../pages/Login';
 import { Register } from '../pages/Register';
+import CategoryPage from '../pages/CategoryPage';
 
 export const MainRoutes = () => {
     return useRoutes([
         { path: "/", element: <MainPage /> },
         { path: "/login", element: <Login /> },
         { path: "/register", element: <Register /> },
+        { path: "/category/:type", element: <CategoryPage /> },
         { path: '*', element: <NotFound /> }
     ])
 }
